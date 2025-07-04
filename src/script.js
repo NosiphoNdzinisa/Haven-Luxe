059b35f03f2d992219152a4a376cb21363a1fb99
NosiphoNdzinisa <nosiphondzinisa@gmail.com>
2025-07-05 00:07:16 +0200

    Added audio and JavaScript prompts

--- src/script.js
+++ src/script.js
@@ -1,3 +1,26 @@
 function greetVisitor() {
     alert("Welcome to my dream home! 🌟 Thank you for visiting 💞");
 }
+
+document.addEventListener("click", function() {
+    const bgMusic = document.getElementById("bg-music");
+    if (music && music.paused){
+        music.play()
+    }  
+},{once:true}
+);
+
+function toggleMusic(){
+    const bgMusic= document.getElementById("bg-music");
+    if (bgMusic.paused) {
+        bgMusic.onplay();
+    }else {
+      bgMusic.pause();  
+    }
+}
+
+AOS.init({
+    duration:1000,
+    easing: 'ease-in-out',
+    once: true
+});
